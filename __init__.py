89abf4c9ba594c9644341f316ff02a195ca96365
Federico Tabbò <federico.tabbo@networktocode.com>
2024-12-20 14:40:24 +0100

    Register in the right place

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from nautobot.apps.jobs import register_jobs
+from training import SiteImportJob
+
+register_jobs(SiteImportJob)
--- jobs/__init__.py
+++
@@ -1,5 +0,0 @@
-from nautobot.apps.jobs import register_jobs
-from .training import SiteImportJob
-
-register_jobs(SiteImportJob)
-
